c2d292c8408b9c7bcdb81d6086494c6d6307977c
Sebastian Zuzunaga <seb55381@hotmail.com>
2023-10-20 02:15:09 -0500

    initial implementation

---
+++ project_undefined.py
@@ -0,0 +1,35 @@
+#Still dont know what to do will start implementing algorithms just in case
+
+#Interval scheduling:
+intervals = [[1,3],[7,12],[2,5],[6,18],[14,16]]
+intervals.sort(key=lambda x: (x[1],x[0]))
+count = 0
+visited = []
+end = -1
+for interval in intervals:
+    if end <= interval[0]:
+        end = interval[1]
+        count += 1
+        visited.append(interval)
+
+print(count, visited)
+#Interval partitioning:
+#need to implement priority queue
+def sortJobs(jobList):
+    return jobList.sort(key = lambda x: x['start'])
+
+def maxEmployeesRequired(jobList):
+	sortedJobList = sortJobs(jobList)
+	customerCare = []
+	for call in sortedJobList:
+		assigned = False
+		for employee in customerCare:
+			if employee['end'] <= call['start']: 
+				assigned = True
+				employee['end'] = call['end']
+				break
+
+		if not assigned:
+			newEmployee = {"end": call['end']}
+			customerCare.append(newEmployee)
+	return len(customerCare)
