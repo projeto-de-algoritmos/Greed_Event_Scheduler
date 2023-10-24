010027b72a4ac904e8ddd52eaf8470643f9bc497
Sebastian Zuzunaga <seb55381@hotmail.com>
2023-10-24 02:29:41 -0500

    added a temporal menu

--- actual_program.py
+++ actual_program.py
@@ -33,26 +33,43 @@ def maxEmployeesRequired(jobList):
     return len(customerCare)
 
 jobList = [{"start": 10, "end": 10.75}, {"start": 10, "end": 11}, {"start": 10, "end": 12}]
-jobList_c = jobList
 
-# Interval scheduling
+while True:
+    print('######Temporary Menu#######')
+    print('Selecione uma opção')
+    print('1.- Inserir uma job')
+    print('2.- Interval scheduling')
+    print('3.- Interval partitioning')
+    print('4.- Sair')
+    option = input()
+    
+    if option == '1':
+        start_t = float(input("Enter start time for job"))
+        end_t = float(input("Enter end time for job"))
+        
+        new_job = {"start": start_t, "end": end_t}
+        jobList.append(new_job)
+    elif option == '2':
+        jobList_c = jobList
+        # Interval scheduling
+        jobList_c.sort(key=lambda x: (x["end"],x["start"]))
+        count = 0
+        visited = []
+        end = -1
+        for job in jobList_c:
+            if end <= job["start"]:
+                end = job["end"]
+                count += 1
+                visited.append(job)
+            
+        print(count, visited)
+    elif option == '3':
+        # Priority queue
+        pq = priorityQueue()
+        for idx, job in enumerate(jobList):
+            pq.push(job, (job["start"], idx))
 
-jobList_c.sort(key=lambda x: (x["end"],x["start"]))
-count = 0
-visited = []
-end = -1
-for job in jobList_c:
-    if end <= job["start"]:
-        end = job["end"]
-        count += 1
-        visited.append(job)
-
-print(count, visited)
-
-# Priority queue
-pq = priorityQueue()
-for idx, job in enumerate(jobList):
-    pq.push(job, (job["start"], idx))
-
-result2 = maxEmployeesRequired(pq)
-print(result2)
+        result2 = maxEmployeesRequired(pq)
+        print(result2)
+    elif option == '4':
+        break
